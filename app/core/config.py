4b80c12eea85d154abdc20eb9e9c96d4cc72ef7d
Naman Singh <namansingh@Namans-Mac-mini.local>
2026-02-23 17:27:28 +0530

    Adding email service config.

--- app/core/config.py
+++ app/core/config.py
@@ -1,57 +1,64 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
-from pydantic import Field,ValidationError,SecretStr
+from pydantic import Field, ValidationError, SecretStr, EmailStr
 from functools import lru_cache
-from typing import List,Optional
+from typing import List, Optional
+
 
 class Settings(BaseSettings):
 
     # api settings
-    API_PREFIX:str = Field(
+    API_PREFIX: str = Field(
         default="/api",
         description="Global api prefix"
     )
 
-    #CORS settings
-    CORS_ALLOW_ORIGINS:List[str] = Field(
+    # CORS settings
+    CORS_ALLOW_ORIGINS: List[str] = Field(
         default_factory=list,
         description="Allowed CORS origins"
     )
-
-    CORS_ALLOW_METHODS:List[str] = Field(
-        default_factory=lambda:["*"]
+    CORS_ALLOW_METHODS: List[str] = Field(
+        default_factory=lambda: ["*"]
     )
-
-    CORS_ALLOW_HEADERS:List[str] = Field(
-        default_factory=lambda:["*"]
+    CORS_ALLOW_HEADERS: List[str] = Field(
+        default_factory=lambda: ["*"]
     )
+    CORS_ALLOW_CREDENTIALS: bool = True
 
-    CORS_ALLOW_CREDENTIALS:bool = True
+    # db settings
+    DB_USER: str = Field(..., description="Database user")
+    DB_PASSWORD: str = Field(..., description="Database pswd")
+    DB_HOST: str = Field(..., description="Database host")
+    DB_PORT: str = Field(..., description="Database port")
+    DB_NAME: str = Field(..., description="Database dbname")
+    SSL_MODE: str = Field(default="require", description="Database connection")
 
-    #db settings
-    DB_USER:str = Field(...,description="Database user")
-    DB_PASSWORD:str = Field(...,description="Database pswd")
-    DB_HOST:str=Field(...,description="Database host")
-    DB_PORT:str=Field(...,description="Database port")
-    DB_NAME:str=Field(...,description="Database dbname")
-    SSL_MODE: str = Field(default="require",description="Database connection")
+    # redis settings
+    REDIS_HOST: str = Field(..., description="Redis host")
+    REDIS_PORT: str = Field(..., description="Redis port")
+    REDIS_DB: int = Field(default=0, description="Type of db")
+    REDIS_PSWD: Optional[str] = None
+    REDIS_USE_SSL: bool = False
 
+    # rate limiting
+    RATE_LIMIT_DEFAULT: str
+    RATE_LIMIT_ENABLED: bool
 
-    #redis settings
-    REDIS_HOST:str = Field(...,description="Redis host")
-    REDIS_PORT:str = Field(...,description="Redis port")
-    REDIS_DB:int = Field(default=0,description="Type of db")
-    REDIS_PSWD:Optional[str] = None
-    REDIS_USE_SSL:bool = False
+    # auth system
+    SECRET_KEY: SecretStr
+    ALGO: str = "HS256"
+    TOKEN_EXPIRE_MIN: int = 30
 
-    #rate limiting 
-    RATE_LIMIT_DEFAULT:str 
-    RATE_LIMIT_ENABLED:bool 
-
-    #auth system
-    SECRET_KEY:SecretStr 
-    ALGO:str = "HS256"
-    TOKEN_EXPIRE_MIN:int = 30
-
+    # mail service
+    MAIL_USERNAME: EmailStr = Field(..., description="Gmail address")
+    MAIL_PASSWORD: SecretStr = Field(..., description="Gmail app password")
+    MAIL_FROM: EmailStr = Field(..., description="Sender email address")
+    MAIL_FROM_NAME: str = Field(
+        default="SMTP", description="Sender display name")
+    SMTP_HOST: str = Field(default="smtp.gmail.com")
+    MAIL_PORT: int = Field(default=587)
+    MAIL_STARTTLS: bool = Field(default=True)
+    MAIL_SSL_TLS: bool = Field(default=False)
 
     model_config = SettingsConfigDict(
         env_file=".env",
@@ -66,9 +73,9 @@ class Settings(BaseSettings):
             f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}"
             f"@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
         )
-    
+
     @property
-    def REDIS_URL(self)->str:
+    def REDIS_URL(self) -> str:
         scheme = "rediss" if self.REDIS_USE_SSL else "redis"
         if self.REDIS_PSWD:
             return (
@@ -82,9 +89,8 @@ class Settings(BaseSettings):
 
 
 @lru_cache(maxsize=1)
-def get_settings() -> Settings :
+def get_settings() -> Settings:
     try:
-        return Settings() #type: ignore[reportCallIssue]
+        return Settings()  # type: ignore[reportCallIssue]
     except ValidationError as e:
         raise RuntimeError("Failed to load the db variables") from e
-    
